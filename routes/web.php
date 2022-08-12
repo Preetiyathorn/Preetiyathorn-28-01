5e76dbc61fb1e531c8a6b5ef061f91be6a3be908
Preetiyathorn Pongsophon <Preetiyathorn@gmail.com>
2022-08-12 20:30:29 +0700

    Template Back End

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
-
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +20,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/about', function() {
+    return "หน้าเกี่ยวกับเรา";
+      });
 
+Auth::routes();
+//profile
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//admin index
+Route::get('/admin/index',[HomeController::class, 'admin'])->name('admin');
+//User
+Route::get('/admin/user/index',[UserController::class,'index'])->name('user.index');
+//category
+Route::get('/admin/category/index',[CategoryController::class, 'index'])->name('category.index');
+Route::get('/admin/category/create',[CategoryController::class, 'createform'])->name('category.create');
+//product
+Route::get('/admin/product/index',[ProductController::class, 'index'])->name('product.index');
+Route::get('/admin/product/create',[ProductController::class, 'createform'])->name('product.create');
